851dc211151991f6540a37fee43b86d28cca587c
Cheulong Sear <cheulong.sear@simyasolutions.com>
2021-09-27 15:32:17 +0700

    add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from 'react';
+import { Provider } from 'react-redux';
 import Router from './route';
+import { store } from './store/store';
 
-const App: FC = () => <Router />;
+const App: FC = () => (
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,36 @@
+/* eslint-disable implicit-arrow-linebreak */
 import React, { FC } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-const Home: FC = () => <div>Home</div>;
+const Home: FC = () => {
+  const counterState = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <button
+        type='button'
+        onClick={() => dispatch({ type: 'counter/decrement' })}
+      >
+        -
+      </button>
+      <button
+        type='button'
+        onClick={() => dispatch({ type: 'counter/increment' })}
+      >
+        +
+      </button>
+      <button
+        type='button'
+        onClick={() =>
+          dispatch({ type: 'counter/incrementByAmount', payload: 5 })
+        }
+      >
+        5
+      </button>
+      <div>Home{counterState.value}</div>
+    </>
+  );
+};
 
 export default Home;
